5383f47faf9540abecb03f6ffdff603075f2e1af
Fernando Garcia <fernando.d.garcia@gmail.com>
2024-08-30 16:53:35 -0300

    java app version 1.0.3

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 rm -rf ./data  
 mkdir ./data  
 chmod 777 ./data
-## Example echo "https://dev.to/chafroudtarek/part-1-how-to-set-up-grafana-and-prometheus-using-docker-i47"
 
 docker-compose up -d ; docker-compose logs -f 
 
