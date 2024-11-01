c095e5ce79d4c1338378166512b88a5338185a31
Fernando Garcia <fernando.d.garcia@gmail.com>
2024-11-01 16:41:46 -0300

    run script

--- k6/run_test.sh
+++ k6/run_test.sh
@@ -4,7 +4,7 @@
 TEST_TO_RUN="./src/script_test_carga_multiples_url_paralelo.js"
 
 
-k6 run -s 15s:500 -s 15s:1000 -s 15s:5000  -s 15s:1000  $TEST_TO_RUN
+k6 run -s 15s:10 -s 30s:500 -s 15s:5000  -s 15s:1000  -s 15s:5000 -s 15s:10  $TEST_TO_RUN
 
 
 
